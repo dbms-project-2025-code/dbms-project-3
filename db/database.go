531a8764831316f891b3a40fa5373f807411fb78
dbms-project-2025-code <dbms.project.mohitsajwan.2025@gmail.com>
2025-10-27 12:53:24 +0530

    files
    files for project

---
+++ db/database.go
@@ -0,0 +1,171 @@
+package db
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+	_ "modernc.org/sqlite"
+)
+
+var db *sql.DB
+
+var users = map[string]string{"admin": "password", "di": "2004", "lian": "3340"} //username : password
+var cookies = map[string]string{"cook": "admin"}                                 //cookies : username
+var data = map[string]string{"admin": "secret", "di": "cookies"}                 //username : secret data
+
+func Connect(path string) {
+	var err error
+	db, err = sql.Open("sqlite", path)
+	if err != nil {
+		log.Fatal(err)
+		//return err
+	}
+	createTables := `
+	CREATE TABLE IF NOT EXISTS credentials (
+		username TEXT PRIMARY KEY,
+		password TEXT NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS sessions (
+		session_id TEXT PRIMARY KEY,
+		username TEXT NOT NULL,
+		expiry TIMESTAMP NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS user_data (
+		username TEXT PRIMARY KEY,
+		data TEXT
+	);`
+	_, err = db.Exec(createTables)
+	if err != nil {
+		log.Panic(err)
+		//return err
+	}
+	insertTestingData()
+	log.Println("Database initialized successfully")
+}
+
+func Close() {
+	err := db.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("Database Connection Closed")
+}
+
+func insertTestingData() {
+	for username, password := range users {
+		hash_pass, e := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+		if e != nil {
+			log.Panic(e)
+		}
+
+		command := "insert or replace into credentials(username, password) values (?,?);"
+		_, err := db.Exec(command, username, string(hash_pass))
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+
+	for username, data := range data {
+		command := "insert or ignore into user_data(username, data) values (?,?);"
+		_, err := db.Exec(command, username, data)
+		if err != nil {
+			log.Panic(err)
+		}
+	}
+}
+
+func Authenticate(username string, password string) bool {
+	query := "SELECT password from credentials where username = ?"
+	row := db.QueryRow(query, username)
+	if row.Err() != nil {
+		log.Panic(row.Err())
+	}
+
+	var hash_pass string
+
+	if err := row.Scan(&hash_pass); err != nil {
+		if err == sql.ErrNoRows {
+			log.Println("No user found with the given ID.")
+		} else {
+			log.Panic(err)
+		}
+		return false
+	}
+	//log.Println(ret_pass)
+	//log.Println(hash_pass)
+	err := bcrypt.CompareHashAndPassword([]byte(hash_pass), []byte(password))
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+func CheckSession(session_id string) (string, error) {
+	query := "Select username from sessions where session_id = ?;"
+	row := db.QueryRow(query, session_id)
+	if row.Err() != nil {
+		log.Panic(row.Err())
+	}
+
+	var username string
+
+	if err := row.Scan(&username); err != nil {
+		if err == sql.ErrNoRows {
+			//log.Println("No user found with the given ID.")
+		} else {
+			log.Panic(err)
+		}
+		return "", errors.New("No User found with the given ID.")
+	}
+	return username, nil
+}
+
+func StoreSession(username string, session_id string) {
+	command := "INSERT OR REPLACE into sessions(session_id, username, expiry) values (?,?,?);"
+	_, err := db.Exec(command, session_id, username, time.Now())
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func GetUserData(username string) (string, error) {
+	query := "SELECT data from user_data where username = ?"
+	row := db.QueryRow(query, username)
+	if row.Err() != nil {
+		log.Panic(row.Err())
+	}
+
+	var data string
+
+	if err := row.Scan(&data); err != nil {
+		if err == sql.ErrNoRows {
+			//log.Println("No user found with the given ID.")
+		} else {
+			log.Println(err)
+		}
+		return "", errors.New("No User data found")
+	}
+	return data, nil
+}
+
+func GarbageCollector() {
+	command := "DELETE FROM sessions where expiry < ?"
+	for {
+		time.Sleep(30 * time.Minute)
+
+		result, err := db.Exec(command, time.Now())
+		if err != nil {
+			log.Panic(err)
+		}
+		rowsAffected, err := result.RowsAffected()
+		if err != nil {
+			log.Panic(err)
+		}
+		log.Printf("Rows affected by delete: %d\n", rowsAffected)
+	}
+}
---
+++ main.go
@@ -0,0 +1,139 @@
+package main
+
+import (
+	"crypto/rand"
+	"dbms/db"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"time"
+)
+
+var users = map[string]string{"admin": "password", "di": "2004"} //username : password
+var cookies = map[string]string{"cook": "admin"}                 //cookies : username
+var data = map[string]string{"admin": "secret"}                  //username : secret data
+
+var tpl = template.Must(template.ParseGlob("./templates/*.html"))
+
+var middleware = []func(http.HandlerFunc) http.HandlerFunc{
+	checkSessionMiddleware,
+}
+
+func main() {
+	db.Connect("./app.db")
+	defer db.Close()
+	go db.GarbageCollector()
+
+	h := http.HandlerFunc(welcomeHandler)
+	for _, m := range middleware {
+		h = m(h)
+	}
+
+	http.HandleFunc("/login", login_page)
+	http.HandleFunc("/", index_page)
+	fmt.Println("Server running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Server is running")
+}
+func login_page(w http.ResponseWriter, r *http.Request) {
+	//users := map[string]string{"Hello": "Templates check"}
+	if r.Method == http.MethodPost {
+		r.ParseForm()
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		if db.Authenticate(username, password) {
+			setSessionID(username, w, r)
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+		} else {
+			tpl.ExecuteTemplate(w, "login.html", map[string]any{"incorrect_pass": true})
+		}
+	} else {
+		tpl.ExecuteTemplate(w, "login.html", nil)
+	}
+}
+
+func index_page(w http.ResponseWriter, r *http.Request) {
+	if username, err := checkSession(r); err == nil {
+		if data, err := db.GetUserData(username); err == nil {
+			fmt.Fprintln(w, data)
+		} else {
+			log.Println(err)
+			fmt.Fprintln(w, "INDEX PAGE")
+
+		}
+	} else {
+		log.Println(err)
+		fmt.Fprintln(w, "INDEX PAGE")
+	}
+}
+
+func checkSession(r *http.Request) (string, error) {
+	//Returns username and check flag true means session found
+	cookie, err := r.Cookie("session_id")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			return "", errors.New("No cookie recieved")
+		}
+		return "", errors.New("Unknown Error With Cookie")
+	}
+	return db.CheckSession(cookie.Value)
+
+}
+
+func checkSessionMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("session_id")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				http.Error(w, "No cookie found", http.StatusBadRequest)
+				return
+			}
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if cookies[cookie.Value] == "" {
+			http.Redirect(w, r, "/", http.StatusSeeOther)
+			return
+		}
+
+		next(w, r)
+	}
+}
+
+func setSessionID(username string, w http.ResponseWriter, r *http.Request) {
+	session_id := generateSessionID()
+	cookie := &http.Cookie{
+		Name:     "session_id",
+		Value:    session_id,
+		Path:     "/", // Cookie is valid for entire site
+		Expires:  time.Now().Add(time.Hour),
+		MaxAge:   3600,  // 1 hour (in seconds)
+		HttpOnly: true,  // Not accessible to JS
+		Secure:   false, // Set to true if using HTTPS
+		SameSite: http.SameSiteLaxMode,
+	}
+	http.SetCookie(w, cookie)
+	db.StoreSession(username, session_id)
+	//fmt.Fprintf(w, "Cookie has been set!")
+}
+
+func generateSessionID() string {
+	id := make([]byte, 32)
+
+	_, err := io.ReadFull(rand.Reader, id)
+	if err != nil {
+		panic("failed to generate session id")
+	}
+
+	return base64.RawURLEncoding.EncodeToString(id)
+}
